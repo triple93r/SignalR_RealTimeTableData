64a26cc0d57bcd488f149fa28085b5b9a5118aa4
triple93r <56282248+triple93r@users.noreply.github.com>
2023-06-17 10:21:36 +0530

    Update ProductRepository.cs
    Graph part removed

--- SignalRDemo3ytEFC/Repositories/ProductRepository.cs
+++ SignalRDemo3ytEFC/Repositories/ProductRepository.cs
@@ -27,58 +27,5 @@ namespace SignalRDemo3ytEFC.Repositories
             //var f = dbContext.Product.ToList();
             return prodList;
         }
-
-        public List<ProductForGraph> GetProductsForGraph()
-        {
-            List<ProductForGraph> productsForGraph = new List<ProductForGraph>();
-            ProductForGraph productForGraph;
-
-            var data = GetProductsForGraphFromDb();
-
-            foreach (DataRow row in data.Rows)
-            {
-                productForGraph = new ProductForGraph
-                {
-                    Category = row["Category"].ToString(),
-                    Products = Convert.ToInt32(row["Products"])
-                };
-                productsForGraph.Add(productForGraph);
-            }
-
-            return productsForGraph;
-        }
-
-        private DataTable GetProductsForGraphFromDb()
-        {
-            var query = "SELECT Category, COUNT(Id) Products FROM Product GROUP BY Category";
-            DataTable dataTable = new DataTable();
-
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                try
-                {
-                    connection.Open();
-                    using (SqlCommand command = new SqlCommand(query, connection))
-                    {
-                        using (SqlDataReader reader = command.ExecuteReader())
-                        {
-                            dataTable.Load(reader);
-                        }
-                    }
-
-                    return dataTable;
-                }
-                catch (Exception ex)
-                {
-                    throw;
-                }
-                finally
-                {
-                    connection.Close();
-                }
-            }
-        }
-
-
     }
 }
